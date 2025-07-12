261c9844258ee515f77632dccf3a7ea2691ff7d1
usman-temp <usmanjaved1620@gmail.com>
2025-07-12 19:05:47 +0500

    Edit src/app/page.js (lines 1-1)

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
-/* eslint-disable */import Header from './components/Header';import HeroSection from './components/HeroSection';import Portfolio from './components/Portfolio';export default function HomePage() {  return (    <main className="min-h-screen">      <Header />      <HeroSection />      <Portfolio />    </main>  );}
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Portfolio from './components/Portfolio';
+
     </main>
   );
 }
