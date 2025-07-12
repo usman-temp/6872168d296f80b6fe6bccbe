65f3304d2c757a9003bb09efba479cdc553bea3a
usman-temp <usmanjaved1620@gmail.com>
2025-07-12 19:15:05 +0500

    Edit next.config.mjs (lines 1-5)

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,1 @@
-/** @type {import('next').NextConfig} */const nextConfig = {  eslint: {    ignoreDuringBuilds: true  }}export default nextConfig;
+const nextConfig = {  eslint: {    ignoreDuringBuilds: true  }};module.exports = nextConfig;
