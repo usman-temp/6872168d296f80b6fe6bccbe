819423882110b35bb307f7e6528a5b099a02487a
usman-temp <usmanjaved1620@gmail.com>
2025-07-12 19:13:51 +0500

    Edit src/app/page.js (lines 0-999)

--- src/app/page.js
+++ src/app/page.js
@@ -38,3 +38,14 @@ export default function HomePage() {
     </main>
   );
 }
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Portfolio from './components/Portfolio';
+export default function HomePage() {
+  return (
+    <main>
+      {' '}
+      <Header /> <HeroSection /> <Portfolio />{' '}
+    </main>
+  );
+}
